dfbe68de5c6fbcce0141a237d27e74d15df0c534
kesh gurung <gurungkesh9@gmail.com>
2024-05-16 09:10:13 +0100

    Made romanized view non editable

--- Roman2Nepali/View/HomeView.swift
+++ Roman2Nepali/View/HomeView.swift
@@ -33,6 +33,7 @@ struct HomeView: View {
                     .bold()
                 
                 CustomTextField(text: $viewModel.nepaliText, placeholder: "")
+                    .disabled(true)
                     
                 HStack {
                     Spacer()
